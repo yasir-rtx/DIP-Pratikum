0262cd86da8ed30a507fe382f416ff8c1023e8ca
yasir <yelfvankarlenstain@gmail.com>
2021-05-31 19:07:27 +0700

    Sharpness | 2:1 | 1:2

--- Image12/Image12/Form1.cs
+++ Image12/Image12/Form1.cs
@@ -110,10 +110,11 @@ namespace Image12
                     int xg8 = (int)((c8.R + c8.G + c8.B) / 3);
                     int xg9 = (int)((c9.R + c9.G + c9.B) / 3);
 
-                    int xt1 = (int)((xg1 + xg2 + xg3 + xg4 + xg5 + xg6 + xg7 + xg8 + xg9) / 9);
-                    int xt2 = (int)((xg1 + xg2 + xg3 + xg4 + xg5 + xg6 + xg7 + xg8 + xg9) / 9);
-                    int xt3 = (int)(-xg1 - 2 * xg4 - xg7 + xg3 + 2 * xg6 + xg9);
+                    int xt1 = (int)((xg1 + xg2 + xg3 + xg4 + xg5 + xg6 + xg7 + xg8 + xg9) / 9); // LPF
+                    int xt2 = (int)(-xg1 - 2 * xg4 - xg7 + xg3 + 2 * xg6 + xg9); // HPF
+                    int xt3 = (int)(-xg1 - 2 * xg4 - xg7 + xg3 + 2 * xg6 + xg9); // HPF
 
+                    // 1 : 2
                     int xn = (int)(2 * (xt1 + xt2 + xt3) / 2);
                     if (xn < 0) xn = -xn;
                     if (xn > 255) xn = 255;
@@ -159,6 +160,7 @@ namespace Image12
                     int xt2 = (int)(-xg1 - 2 * xg4 - xg7 + xg3 + 2 * xg6 + xg9);
                     int xt3 = (int)(-xg1 - 2 * xg4 - xg7 + xg3 + 2 * xg6 + xg9);
 
+                    // 2 : 1
                     int xn = (int)(2 * (xt1 + (2 * xt2) + (2 * xt3)) / 3);
                     if (xn < 0) xn = -xn;
                     if (xn > 255) xn = 255;
